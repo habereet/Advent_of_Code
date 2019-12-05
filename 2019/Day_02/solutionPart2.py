8506390be6e0b7697a3f54c5361db0975170f11d
habereet <ubuntu@ip-172-31-38-210.ec2.internal>
2019-12-05 13:45:20 +0000

    refactored code to remove nested for loops and unneeded value

--- 2019/Day_02/solutionPart2.py
+++ 2019/Day_02/solutionPart2.py
@@ -1,5 +1,6 @@
 import sys
 from os import path
+from itertools import product
 
 sys.path.insert(1, f"..{path.sep}..{path.sep}Data")
 from getData import *
@@ -13,28 +14,24 @@ else:
     originalData = read_data()
 
 originalData = [int(x) for x in originalData[0].split(',')]
-foundInputs = False
 try:
-    for noun in range(100):
-        for verb in range(100):
-            data = originalData.copy()
-            data[1] = noun
-            data[2] = verb
-            count = 0
-            while True:
-                action = data[count]
-                index_one = count + 1
-                index_two = count + 2
-                store_index = count + 3
-                if action != 1 and action != 2:
-                    break
-                data[data[store_index]] = data[data[index_one]] + data[data[index_two]] if action == 1 else data[data[index_one]] * data[data[index_two]]
-                count += 4
+    for noun, verb in product(range(100), range(100)):
+        data = originalData.copy()
+        data[1] = noun
+        data[2] = verb
+        count = 0
+        while True:
+            action = data[count]
+            index_one = count + 1
+            index_two = count + 2
+            store_index = count + 3
+            if action != 1 and action != 2:
+                break
+            data[data[store_index]] = data[data[index_one]] + data[data[index_two]] if action == 1 else data[data[index_one]] * data[data[index_two]]
+            count += 4
             if data[0] == 19690720:
-                foundInputs = True
                 raise StopIteration
 except StopIteration:
-    print(f'{noun}{verb}')
+    print(f'{noun*100 + verb}')
     exit(0)
-if foundInputs == False:
-    print("Couldn't identify inputs")
+print("Couldn't identify inputs")
